9ed11b3da13a9341c5ba744b8cd2b658c5aead4c
Boretsky <jonbor@amazon.com>
2017-10-21 21:45:30 -0700

    update audio recording to upload continuous clips

--- Dubhacks/Assets/Recognition.cs
+++ Dubhacks/Assets/Recognition.cs
@@ -6,13 +6,11 @@ public class Recognition : MonoBehaviour {
 
 	AudioSource _audio;
 	AudioSource _audio2;
+	int i = 0;
 
 	// Use this for initialization
 	void Start () {
 		_audio = GetComponent<AudioSource>();
-//		_audio.mute = true;
-//		while (!(Microphone.GetPosition(null) > 0)) {}
-//		_audio.Play();
 		UpdateAudio();
 	}
 
@@ -22,29 +20,15 @@ public class Recognition : MonoBehaviour {
 		_audio.loop = true;
 
 		Debug.Log (Microphone.IsRecording ("Built-in Microphone").ToString ());
-
-		if (Microphone.IsRecording("Built-in Microphone")) {
-			while (!(Microphone.GetPosition (null) > 0)) {}
-		}
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (!(Microphone.IsRecording ("Built-in Microphone")) && !_audio.isPlaying) {
-			Debug.Log ("POOP");
-			_audio.Play();
-			SavWav.Save("clip", _audio.clip);
-//			_audio.clip = Microphone.Start("Built-in Microphone", false, 5, 44100);
+//			_audio.Play();
+			++i;
+			SavWav.Save("clip"+i, _audio.clip);
+			_audio.clip = Microphone.Start("Built-in Microphone", false, 5, 44100);
 		}
-	}
-
-	float getAveragedVolume() {
-		float[] data = new float[256];
-		float a = 0;
-		_audio.GetOutputData(data, 0);
-		foreach(float s in data){
-			a+=Mathf.Abs(s);
-		}
-		return a / 256;
 	}
 }
