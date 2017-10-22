61aad298b18fab07d3c10ea13f263266e8da4983
Boretsky <jonbor@amazon.com>
2017-10-22 04:34:18 -0700

    Add async speech to text

--- Dubhacks/Assets/Recognition.cs
+++ Dubhacks/Assets/Recognition.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEditor.Networking;
 using System.Net;
 using System.IO;
+using System.ComponentModel;
+using System.Text;
 using System;
 
 [Serializable]
@@ -31,12 +33,18 @@ public class Recognition : MonoBehaviour {
 	AudioSource _audio;
 	AudioSource _audio2;
 	TextMesh _text;
+	public BackgroundWorker backgroundWorker1;
 	int i = 0;
 
 	// Use this for initialization
 	void Start () {
 		_audio = GetComponent<AudioSource>();
 		_text = GetComponent<TextMesh>();
+
+		backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+		backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Send);
+		backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.Receive);
+
 		UpdateAudio();
 	}
 
@@ -45,7 +53,6 @@ public class Recognition : MonoBehaviour {
 		_audio.clip = Microphone.Start("Built-in Microphone", false, 3, 44100);
 		_audio.loop = true;
 
-//		Debug.Log (Microphone.IsRecording ("Built-in Microphone").ToString ());
 		if (Microphone.IsRecording("Built-in Microphone")) {
 			while (!(Microphone.GetPosition (null) > 0)) {}
 		}
@@ -58,7 +65,7 @@ public class Recognition : MonoBehaviour {
 				++i;
 				SavWav.Save("clip"+i, _audio.clip);
 				Debug.Log ("Saved audio clip" + i);
-				Send(Application.dataPath+"/clip"+i+".wav");
+				this.backgroundWorker1.RunWorkerAsync(Application.dataPath+"/clip"+i+".wav");
 				_audio.clip = Microphone.Start("Built-in Microphone", false, 3, 44100);
 			}
 			catch(Exception e) {
@@ -67,99 +74,86 @@ public class Recognition : MonoBehaviour {
 		}
 	}
 
-	void Send(string filePath) {
+	void Receive(object sender, RunWorkerCompletedEventArgs e) {
+		_text.text = (string)e.Result;
+
+	}
+
+	void Send(object sender, DoWorkEventArgs e) {
 		HttpWebRequest request = null;
-		request = (HttpWebRequest)HttpWebRequest.Create("https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?language=en-US&format=detailed");
+		request = (HttpWebRequest)HttpWebRequest.Create ("https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?language=en-US&format=detailed");
 		request.SendChunked = true;
 		request.Accept = @"application/json;text/xml";
 		request.Method = "POST";
 		request.ProtocolVersion = HttpVersion.Version11;
 		request.ContentType = @"audio/wav; codec=audio/pcm; samplerate=16000";
-		request.Headers["Ocp-Apim-Subscription-Key"] = "eea31b8c6a2041a59af19b436dd372d7";
+		request.Headers ["Ocp-Apim-Subscription-Key"] = "eea31b8c6a2041a59af19b436dd372d7";
 
 		// Send an audio file by 1024 byte chunks
-		using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-		{
+		using (var fs = new FileStream ((string)e.Argument, FileMode.Open, FileAccess.Read)) {
 
 			/*
     		* Open a request stream and write 1024 byte chunks in the stream one at a time.
     		*/
 			byte[] buffer = null;
 			int bytesRead = 0;
-			using (Stream requestStream = request.GetRequestStream())
-			{
+			using (Stream requestStream = request.GetRequestStream ()) {
 				/*
         		* Read 1024 raw bytes from the input audio file.
         		*/
-				buffer = new Byte[checked((uint)Math.Min(1024, (int)fs.Length))];
-				while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) != 0)
-				{
-					requestStream.Write(buffer, 0, bytesRead);
+				buffer = new Byte[checked((uint)Math.Min (1024, (int)fs.Length))];
+				while ((bytesRead = fs.Read (buffer, 0, buffer.Length)) != 0) {
+					requestStream.Write (buffer, 0, bytesRead);
 				}
 
 				// Flush
-				requestStream.Flush();
+				requestStream.Flush ();
 			}
 		}
 
-		using (WebResponse response = request.GetResponse()) {
+		using (WebResponse response = request.GetResponse ()) {
 			string responseString;
-			Debug.Log(((HttpWebResponse)response).StatusCode);
+			Debug.Log (((HttpWebResponse)response).StatusCode);
 
-			using (StreamReader sr = new StreamReader(response.GetResponseStream())) {
-				responseString = sr.ReadToEnd();
+			using (StreamReader sr = new StreamReader (response.GetResponseStream ())) {
+				responseString = sr.ReadToEnd ();
 			}
-			responseString = responseString.Replace("N-Best", "NBest");
+			responseString = responseString.Replace ("N-Best", "NBest");
 
-			TextResponse ret = TextResponse.CreateFromJSON(responseString);
+			TextResponse ret = TextResponse.CreateFromJSON (responseString);
 			if (ret.RecognitionStatus == "Success") {
 				if (ret.NBest.Length > 0) {
 					N_Best final = ret.NBest [0];
-					_text.text = final.Display;
+					string result = final.Display;
+					e.Result = WrapString(result);
 				} else {
 					Debug.Log ("No response");
 				}
 			}
 		}
-
-//		// init your request...then:
-//		DoWithResponse(request, (response) => {
-//			string responseString;
-//			using (StreamReader sr = new StreamReader(response.GetResponseStream()))
-//			{
-//				responseString = sr.ReadToEnd();
-//	//			_text.text = sr.ReadToEnd();
-//			}
-//			Debug.Log(responseString);
-//		});
 	}
-//
-//	void DoWithResponse(HttpWebRequest request, Action<HttpWebResponse> responseAction) {
-//		Action wrapperAction = () =>
-//		{
-//			request.BeginGetResponse(new AsyncCallback((iar) =>
-//				{
-//					var response = (HttpWebResponse)((HttpWebRequest)iar.AsyncState).EndGetResponse(iar);
-//					responseAction(response);
-//				}), request);
-//		};
-//		wrapperAction.BeginInvoke(new AsyncCallback((iar) =>
-//			{
-//				var action = (Action)iar.AsyncState;
-//				action.EndInvoke(iar);
-//			}), wrapperAction);
-//	}
-}
 
-//public static class JsonHelper {
-//
-//	public static T[] FromJson<T>(string json) {
-//		Wrapper<T> wrapper = UnityEngine.JsonUtility.FromJson<Wrapper<T>>(json);
-//		return wrapper.Items;
-//	}
-//
-//	[Serializable]
-//	private class Wrapper<T> {
-//		public T[] T;
-//	}
-//}
+	public string WrapString(string str) {
+		int myLimit = 20;
+		string[] words = str.Split(' ');
+
+		StringBuilder newSentence = new StringBuilder();
+
+		string line = "";
+		foreach (string word in words)
+		{
+			if ((line + word).Length > myLimit)
+			{
+				newSentence.AppendLine(line);
+				line = "";
+			}
+
+			line += string.Format("{0} ", word);
+		}
+
+		if (line.Length > 0)
+			newSentence.AppendLine(line);
+
+		return newSentence.ToString();
+	}
+}
